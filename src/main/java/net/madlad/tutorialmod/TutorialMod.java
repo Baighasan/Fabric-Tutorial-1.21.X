5aee2dcabcc33aafc278e8e4dff71fbd3dbe2ae4
Baighasan <baig.hasan@outlook.com>
2025-02-28 14:50:10 -0500

    Added a custom food and fuel source

--- src/main/java/net/madlad/tutorialmod/TutorialMod.java
+++ src/main/java/net/madlad/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.madlad.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.madlad.tutorialmod.block.ModBlocks;
 import net.madlad.tutorialmod.item.ModItemGroups;
 import net.madlad.tutorialmod.item.ModItems;
@@ -18,5 +19,7 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 	}
 }
